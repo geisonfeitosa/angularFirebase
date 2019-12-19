0f2e58ba22e612266a0087e1a4312b170d407674
root <geisonlealfeitosa.glf@gmail.com>
2019-12-19 16:42:29 -0300

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,14 +2,7 @@ export const environment = {
   production: true,
 
   firebaseConfig: {
-    apiKey: "AIzaSyDhZsqljpfYqzWExTTSwiH4oEnN5Xig_Mw",
-    authDomain: "angularfirebase-baeab.firebaseapp.com",
-    databaseURL: "https://angularfirebase-baeab.firebaseio.com",
-    projectId: "angularfirebase-baeab",
-    storageBucket: "angularfirebase-baeab.appspot.com",
-    messagingSenderId: "734601727542",
-    appId: "1:734601727542:web:efce4f3eb9f5c38c4a5297",
-    measurementId: "G-QR4C0TB9ZE"
+    //dados firebase aqui
   }
-  
+
 };
